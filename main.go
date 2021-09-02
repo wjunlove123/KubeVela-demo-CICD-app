94240b6dede3042e302c151a326a69339c45140b
Yin Da <yd219913@alibaba-inc.com>
2021-09-02 15:33:32 +0800

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+// main.go
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const VERSION = "0.1.0-v1alpha1"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "Version: %s\n", VERSION)
+	})
+	if err := http.ListenAndServe(":8088", nil); err != nil {
+		println(err.Error())
+	}
+}
